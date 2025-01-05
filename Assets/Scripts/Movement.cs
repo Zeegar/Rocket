8b73b9b6498e7fa14f3310c3432f39eb21880c5c
pez war <powerhouse.pear.bear@gmail.com>
2025-01-05 12:20:23 +0000

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,74 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float maxSpeed = 50f;
+    [SerializeField] float acceleration = 20f;
+    [SerializeField] float deceleration = 10f;
+    [SerializeField] float rotationSpeed = 129f;
+    [SerializeField] float gravityScale = 0.02f; // Serialized variable to control gravity
+
+    private float currentSpeed = 0f;
+    private Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.useGravity = false; // Disable default gravity
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thruster();
+        Rotation();
+        ApplyCustomGravity();
+    }
+
+    void Thruster()
+    {
+        // Move object in the object's y axis
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentSpeed += acceleration * Time.deltaTime;
+            currentSpeed = Mathf.Clamp(currentSpeed, 0f, maxSpeed);
+        }
+        else
+        {
+            currentSpeed -= deceleration * Time.deltaTime;
+            currentSpeed = Mathf.Clamp(currentSpeed, 0f, maxSpeed);
+        }
+
+        transform.Translate(0f, currentSpeed * Time.deltaTime, 0f);
+    }
+
+
+    void Rotation()
+    {
+        rb.freezeRotation = true;
+        // Rotate object in the object's Z axis
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && !(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
+        }
+        else if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)))
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * rotationSpeed);
+        }
+        rb.freezeRotation = false;
+    }
+
+    void ApplyCustomGravity()
+    {
+        if (rb != null)
+        {
+            Vector3 gravity = Physics.gravity * gravityScale;
+            rb.AddForce(gravity, ForceMode.Acceleration);
+        }
+    }
+}
